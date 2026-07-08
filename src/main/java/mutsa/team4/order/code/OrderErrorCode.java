de37234aa97bb213590120d5a44ab806a9ed637d
KangSangJun <befur1@g.hongik.ac.kr>
2026-07-08 15:10:12 +0900

    [feat] 주문 에러케이스 추가

--- src/main/java/mutsa/team4/order/code/OrderErrorCode.java
+++ src/main/java/mutsa/team4/order/code/OrderErrorCode.java
@@ -12,7 +12,9 @@ public enum OrderErrorCode implements BaseErrorCode {
     INSUFFICIENT_BALANCE(HttpStatus.BAD_REQUEST, "ORDER400_2", "잔액이 부족합니다."),
     STORE_CLOSED(HttpStatus.BAD_REQUEST, "ORDER400_3", "현재 영업시간이 아니므로 주문할 수 없습니다."),
 
-    CREDIT_NOT_FOUND(HttpStatus.BAD_REQUEST, "ORDER404_1", "결제 계좌를 찾을 수 없습니다.");
+    CREDIT_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER404_1", "결제 계좌를 찾을 수 없습니다."),
+    CART_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER404_2", "장바구니 정보를 찾을 수 없습니다"),
+    MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "ORDER404_3", "메뉴 정보를 찾을 수 없습니다");
 
     private final HttpStatus httpStatus;
     private final String code;
