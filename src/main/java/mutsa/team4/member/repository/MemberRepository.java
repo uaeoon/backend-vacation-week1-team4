7fdd6037fb6af753519c291895d082a6169ce25c
KangSangJun <befur1@g.hongik.ac.kr>
2026-07-13 04:01:49 +0900

    [feat] 멤버 repository 구현

---
+++ src/main/java/mutsa/team4/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package mutsa.team4.member.repository;
+
+import mutsa.team4.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
