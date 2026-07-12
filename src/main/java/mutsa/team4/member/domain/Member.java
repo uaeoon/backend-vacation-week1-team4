77e163103f3f5b50f7772b6cd4be1f8178adb959
KangSangJun <befur1@g.hongik.ac.kr>
2026-07-13 04:01:31 +0900

    [feat] 멤버 도메인 구현

---
+++ src/main/java/mutsa/team4/member/domain/Member.java
@@ -0,0 +1,29 @@
+package mutsa.team4.member.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class Member {
+    @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    public static Member createMember(String name, String email, String password) {
+        return Member.builder()
+                .name(name)
+                .email(email)
+                .password(password)
+                .build();
+    }
+}
