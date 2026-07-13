72b3f463cd3160f7c6d0e5d8f4ae7a8b71148c95
uaeoon <hhaaee9153@g.hongik.ac.kr>
2026-07-13 20:25:01 +0900

    [refactor] 기존 API 인증 사용자 기반으로 수정

--- src/main/java/mutsa/team4/cart/controller/CartController.java
+++ src/main/java/mutsa/team4/cart/controller/CartController.java
@@ -6,6 +6,8 @@ import mutsa.team4.cart.dto.CartRequestDto;
 import mutsa.team4.cart.dto.CartResponseDto;
 import mutsa.team4.cart.service.CartService;
 import mutsa.team4.global.apiPayload.ApiResponse;
+import mutsa.team4.global.security.AuthMember;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,35 +18,43 @@ public class CartController {
 
     //장바구니 조회 API
     @GetMapping
-    public ApiResponse<CartResponseDto.CartInfoResponseDto> getCart() {
-        //memberId 임시 설정 추후 변경 필요
-        Long memberId = 1L;
+    public ApiResponse<CartResponseDto.CartInfoResponseDto> getCart(
+            @AuthenticationPrincipal AuthMember authMember
+    ) {
+        Long memberId = authMember.getMemberId();
         CartResponseDto.CartInfoResponseDto response = cartService.getCart(memberId);
         return ApiResponse.onSuccess("장바구니 조회 성공", response);
     }
 
     //장바구니 담기 (메뉴 추가) API
     @PostMapping("/items")
-    public ApiResponse<Void> addCartItem(@RequestBody@Valid CartRequestDto.AddCartItemRequestDto requestDto){
-        //memberId 임시 설정
-        Long memberId = 1L;
+    public ApiResponse<Void> addCartItem(
+            @AuthenticationPrincipal AuthMember authMember,
+            @RequestBody@Valid CartRequestDto.AddCartItemRequestDto requestDto
+    ) {
+        Long memberId = authMember.getMemberId();
         cartService.addCartItem(memberId, requestDto);
         return ApiResponse.onSuccess("장바구니에 상품을 성공적으로 담았습니다");
     }
     //장바구니 수량 변경
     @PatchMapping("/items/{cartItemId}")
-    public ApiResponse<Void> updateCartItemQuantity(@PathVariable("cartItemId") Long cartItemId, @RequestBody@Valid CartRequestDto.UpdateQuantity requestDto){
-        //memberId 임시 설정
-        Long memberId = 1L;
+    public ApiResponse<Void> updateCartItemQuantity(
+            @AuthenticationPrincipal AuthMember authMember,
+            @PathVariable("cartItemId") Long cartItemId,
+            @RequestBody@Valid CartRequestDto.UpdateQuantity requestDto
+    ) {
+        Long memberId = authMember.getMemberId();
         cartService.updateCartItemQuantity(memberId, cartItemId, requestDto);
         return ApiResponse.onSuccess("상품 수량이 변경되었습니다.");
 
     }
 
     @DeleteMapping("/items/{cartItemId}")
-    public ApiResponse<Void> deleteCartItem(@PathVariable("cartItemId") Long cartItemId){
-        //memberId 임시 설정
-        Long memberId = 1L;
+    public ApiResponse<Void> deleteCartItem(
+            @AuthenticationPrincipal AuthMember authMember,
+            @PathVariable("cartItemId") Long cartItemId
+    ) {
+        Long memberId = authMember.getMemberId();
         cartService.deleteCartItem(memberId, cartItemId);
         return ApiResponse.onSuccess("장바구니에서 상품을 삭제했습니다.");
     }
